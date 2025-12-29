8ebd0dd18323d876eabe7526f541951ac89dd76f
mahmoudeissa9 <mahmoudeissami10@gmail.com>
2025-12-28 22:20:45 -0500

    handling select,delete and other featurs

--- menu_script.sh
+++ menu_script.sh
@@ -126,6 +126,257 @@ insert_into_table()
 }                    
 
 #############################################################################
+                       # select from table #
+#############################################################################
+Select_From_table(){
+read -p "Enter name of the table you want select from " tablename
+if [ ! -f $current_db/$tablename ];
+then 
+echo "This table not exist"
+return
+fi
+
+IFS='|' read -r -a metadataarray < "$current_db/$tablename.metadata"
+read -p "enter the number of coloums u want seclect if you want to select all colums enter 'all' " colnum 
+
+if [ $colnum == "all" ];
+then
+wherefunc "$tablename" "all"
+return
+fi
+if ! [[ "$colnum" =~ ^[0-9]+$ ]];
+then 
+echo " invalid input , enter number or 'all' "
+return 
+fi
+
+col=1
+coloumsindex=""
+while [ $col -le $colnum ]
+do
+read -p "enter the colum $col name " colname
+foundindex=""
+idx=1
+for meta in "${metadataarray[@]}" ; 
+do 
+IFS=':' read -r name _ _ <<< "$meta"
+if [ "$name" == "$colname" ] ;
+then 
+foundindex=$idx 
+break 
+fi 
+((idx++))
+done 
+if [ -z "$foundindex" ];
+then 
+echo "coloum not found " 
+return 
+fi 
+ 
+   if [ -z "$coloumsindex" ];
+    then 
+    coloumsindex="$foundindex"
+    else 
+    coloumsindex+=",$foundindex"
+    fi
+
+   (( col++ ))
+done 
+wherefunc "$tablename" "$coloumsindex"
+
+}
+###################### where func #################
+
+wherefunc() {
+    local tablename=$1
+    local coloumsindex=$2
+
+    
+    IFS='|' read -r -a metadataarray < "$current_db/$tablename.metadata"
+    
+
+    local full_header=""
+    for meta in "${metadataarray[@]}"; do
+        IFS=':' read -r name _ _ <<< "$meta"
+        if [ -z "$full_header" ]; then
+            full_header="$name"
+        else
+            full_header="$full_header:$name"
+        fi
+    done
+
+    local final_header=""
+    if [ "$coloumsindex" == "all" ]; then
+        final_header="$full_header"
+    else
+        final_header=$(echo "$full_header" | cut -d':' -f"$coloumsindex")
+    fi
+
+    read -p "enter condition col name (press enter if you don't need condition) " condcol
+    
+    {
+        echo "$final_header"
+    
+        if [ -z "$condcol" ]; then 
+            if [ "$coloumsindex" == "all" ]; then 
+                cat "$current_db/$tablename"
+            else 
+                cut -d':' -f"$coloumsindex" "$current_db/$tablename"
+            fi
+        else 
+            read -p "enter value for $condcol: " condvalue
+            condindex=""
+            idx=1
+            for meta in "${metadataarray[@]}"; do 
+                IFS=':' read -r name _ _ <<< "$meta"
+                if [ "$name" == "$condcol" ]; then 
+                    condindex=$idx
+                    break 
+                fi
+                ((idx++))
+            done
+            if [ -z "$condindex" ]; then 
+                echo "condition col not found " >&2 
+                return 
+            fi 
+            
+            if [ "$coloumsindex" == "all" ]; then 
+                awk -F: -v c_idx="$condindex" -v c_val="$condvalue" '$c_idx == c_val' "$current_db/$tablename"
+            else 
+                awk -F: -v c_idx="$condindex" -v c_val="$condvalue" '$c_idx == c_val' "$current_db/$tablename" | cut -d':' -f"$coloumsindex"
+            fi
+        fi
+
+    } | column -t -s ':' -o ' | ' 
+ 
+
+
+}
+
+
+
+
+##############################################################################
+                               # delete #
+##############################################################################
+delete_from_table()
+{
+read -p "Enter name of the table you want to delete from " tablename
+if [ ! -f $current_db/$tablename ];
+then 
+echo "This table not exist"
+return
+fi
+while true;
+do 
+read -p "enter condition col name (press enter if you don't need condition) " condcol
+if [ -z "$condcol" ];
+then
+read -p "you will delete all table data [y/n] : " ans
+if   [ "$ans" == "y" ];
+then
+  > "$current_db/$tablename"
+  echo "Table data is deleted successfully"
+return
+fi
+
+else
+
+ IFS='|' read -r -a metadataarray < "$current_db/$tablename.metadata"
+
+            
+            condindex=""
+            idx=1
+            for meta in "${metadataarray[@]}"; do 
+                IFS=':' read -r name _ _ <<< "$meta"
+                if [ "$name" == "$condcol" ]; then 
+                    condindex=$idx
+                    break 
+                fi
+                ((idx++))
+            done
+            if [ -z "$condindex" ]; then 
+                echo "condition col not found "
+                continue
+            fi 
+             read -p "enter value for $condcol: " condvalue
+
+ awk -F: -v idx="$condindex" -v val="$condvalue" ' $idx != val '  "$current_db/$tablename" > "$current_db/$tablename.tmp"
+
+mv "$current_db/$tablename.tmp" "$current_db/$tablename"
+
+echo "Rows deleted successfully"
+
+
+return
+fi
+done
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###############################################################################
+                                # update # 
+###############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###########################################
+connect_display() 
+{
+    echo "------------------------------------------------------------"
+ echo -e " 1) "Create Table"\t2) "List Table"\t   \t3) "Drop Table"\t4) "Insert into Table"\n 5) "Select from Table"\t6) "Delete from Table"\t7) "Update Table"\t8) "Quit""
+}
+main_display()
+{
+     echo "------------------------------------------------------------"
+    echo -e " 1) "Create_Database"\t 2) "List_Database"\n 3) "Connect_Database"\t 4) "Drop_Database"\t 5) "Quit""
+}
+
+
+
+
+
+###################################################################
+                          # main menu #
+###################################################################
 mkdir -p DBS_dir
 echo "choose from the following"
 PS3="enter your choice " 
@@ -139,11 +390,13 @@ do
         else mkdir ./DBS_dir/$dbname
         echo "data base created successfully " 
         fi
+        main_display
         ;;
         ####################################################################
         List_Database)
         echo " the list of data bases " 
         ls ./DBS_dir 
+        main_display
         ;;
         #######################################################################
         Connect_Database)
@@ -154,12 +407,15 @@ do
            select choice in "Create Table" "List Table" "Drop Table" "Insert into Table" "Select from Table" "Delete from Table" "Update Table" "Quit"
           do 
           case $REPLY in
-          1) createTable ;;
+          1) createTable 
+          connect_display
+          ;;
 
     #####################################
-          2) echo " list table" 
-          echo "List of Tables:"
+          2)
+            echo "List of Tables:"
             ls "$current_db" | grep -v ".metadata"
+            connect_display
                  ;;
     ################################################
     ## 
@@ -171,17 +427,26 @@ do
           else 
           echo "this table not exist " 
           fi
-          
-          
+          connect_display
           ;;
 
-          4) insert_into_table ;;
+          4) insert_into_table
+         connect_display
+           ;;
 
-          5)echo "select from table " ;;
-          6)echo "delete table " ;;
-          7)echo "updata" ;;
+          5) Select_From_table 
+          connect_display
+          ;;
+          6)delete_from_table
+          connect_display
+          ;;
+          7)echo "updata" 
+          connect_display
+          ;;
           8) break ;;
-          *) echo "invalid option " ;;
+          *) echo "invalid option " 
+          connect_display
+          ;;
 #######################################
           esac 
           done 
@@ -191,15 +456,24 @@ do
 
           fi
 
-          
+          main_display
         ;;
         
-        Drop_Database);;
+        Drop_Database)
+        read -p "enter name of DB you want to drop "  dbname 
+        if [ ! -d "./DBS_dir/$dbname" ];
+        then echo "this data base not exist "
+        else rm -r ./DBS_dir/$dbname
+        echo "data base dropped successfully " 
+        fi
+         main_display
+         ;;
         Quit)
             break
             ;;
         *) 
             echo "Invalid option" 
+            main_display
             ;;
         
     esac
